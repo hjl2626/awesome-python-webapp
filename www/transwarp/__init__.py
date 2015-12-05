0b268fbd8700367eba03e954c9b58d375d3a0608
hjl2626 <799728970@qq.com>
2015-12-05 17:10:28 +0800

    develop db and rom moudle

---
+++ www/transwarp/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""
+@version: ??
+@site: 
+@software: PyCharm
+@file: __init__.py.py
+@time: 2015/12/4 23:09
+"""
+
+if __name__ == '__main__':
+    pass
---
+++ www/transwarp/test.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+"""
+@version: ??
+@site: 
+@software: PyCharm
+@file: test.py
+@time: 2015/12/5 12:35
+"""
+import logging, time
+import db
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    db.create_engine('root', '123456', 'test')
+    # db.update('drop table if exists user')
+    # db.update('create table user (id VARCHAR(50) primary key, name text, email text, passwd text, last_modified real)')
+    # u1 = dict(id=db.next_id(), name='Michael', email='michael@test.org', passwd='123456', last_modified=time.time())
+    # db.insert('user', **u1)
+    # rs=db.select('select * from user where name=?', 'Michael')
+    # for r in rs:
+    #     for key,value in r.iteritems():
+    #         print key,value
+    #     print '\n\n'
